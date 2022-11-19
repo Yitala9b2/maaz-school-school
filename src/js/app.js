0db93817fbc061fd5507a35ab8931cd4e2abf6e3
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2022-11-19 17:18:10 +0300

    scroll

--- src/js/app.js
+++ src/js/app.js
@@ -161,7 +161,7 @@ flsScroll.pageNavigation();
 
 // Функционал добавления классов к хедеру при прокрутке
 // Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
-// flsScroll.headerScroll();
+ flsScroll.headerScroll();
 
 // Функционал липкого блока
 // flsScroll.stickyBlock();
